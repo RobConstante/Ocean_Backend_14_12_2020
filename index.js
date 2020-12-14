55f8c3c45715da60366a5eec76b1be52cb321edf
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-14 14:50:18 -0300

    Implementação do endpoint de read all e create e do bodyParser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,14 +13,23 @@ app.get('/', (req, res) => {
 // Create, Read (All or Single), Update, Delete
 // Criar, Ler (Tudo ou Individual), Atualizar e Remover
 
+const mensagens = [
+  'Essa é a primeira mensagem',
+  'Essa é a segunda mensagem'
+];
+
 // [READ] All - Ler todas as mensagens
 app.get('/mensagens', (req, res) => {
-  res.send('Exibir a lista de mensagens.');
+  res.send(mensagens);
 });
 
 // [CREATE] - Criar uma mensagem
 app.post('/mensagens', (req, res) => {
-  res.send('Criar uma mensagem.');
+  const texto = req.body.texto;
+
+  mensagens.push(texto);
+
+  res.send('Mensagem foi criada com sucesso!');
 });
 
 app.listen(3000, () => {
